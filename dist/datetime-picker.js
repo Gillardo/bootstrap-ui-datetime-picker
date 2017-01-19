57c51a4efe76a5a33b1d848e39eb1e0fbb0dbe32
Gillardo <darren.gillard81@gmail.com>
2017-01-19 09:17:14 +0000

    Release 2.5.2

--- dist/datetime-picker.js
+++ dist/datetime-picker.js
@@ -1,5 +1,5 @@
 // https://github.com/Gillardo/bootstrap-ui-datetime-picker
-// Version: 2.5.1
+// Version: 2.5.2
 // Released: 2017-01-19 
 angular.module('ui.bootstrap.datetimepicker', ['ui.bootstrap.dateparser', 'ui.bootstrap.position'])
     .constant('uiDatetimePickerConfig', {
